bd7f4fe3f33a4d8cc9f5dc04c398943ec94cdfb4
Sancaknr <143341231+Sancaknr@users.noreply.github.com>
2023-08-30 16:04:46 +0300

    Advanced Calculator v2.
    Bug fixes, maked more useful.

---
+++ useful_things_python/calculator_advanced_2.py
@@ -0,0 +1,113 @@
+#Calculate the numbers using signs
+import re
+
+def is_float(n):
+    if "." in n:
+        float_n = float(n)
+        return True
+    else:
+        try:
+            int_n = int(n)
+            return False
+        except ValueError:
+            return False
+
+def Calculator():
+    sign = input("1-> '+'     2-> '-'     3-> '*'  4-> '/'    5-> '**' \n   Select your sign using a number: ")
+    error_sign = 0
+    error_process = 0
+    error_process2 = 0
+    sign_progress = False
+    while sign in ["1", "2", "3", "4", "5"] or sign_progress == True:
+        if error_process == 0 and error_process2 == 0:
+            process = input("You selected your sign, please write first number to calculate: ")
+        if (process) != "" and (process.isdigit() or is_float(process)):
+            if error_process2 == 0:
+                process2 = input("Please write another number for calculate: ")
+            try:
+                if ((process2) != "") and (is_float(process) or is_float(process2) == True):
+                    if float(process) % float(process2) == 0 and not float(process2) % float(process) == 0:
+                        print("This equals {:,}".format(int(float(process) / float(process2))))
+                        break
+                    elif float(process2) % float(process) == 0:
+                        print("This equals {:,}".format(int(float(process2) / float(process))))
+                        break
+                    if sign == "1":
+                        print("This equals {:.2f}".format(float(process) + float(process2)))
+                        break
+                    if sign == "2":
+                        print("This equals {:.2f}".format(float(process) - float(process2)))
+                        break
+                    if sign == "3":
+                        print("This equals {:.2f}".format((float(process) * float(process2))))
+                        break
+                    if sign == "4":
+                        print("This equals {:.2f}".format((float(process) / float(process2))))
+                        break
+                    if sign == "5":
+                        print("This equals {:.2f}".format((float(process) ** float(process2))))
+                        break
+                elif (process2) == "":
+                    if error_process2 != 3:
+                        process2 = input("Please write a number for calculate: ")  
+                        error_process2 += 1
+                    else:
+                        print("Please try again later.")
+                        break
+                else:
+                    if sign == "1":
+                        if re.search(r"\d+\.\d{2,}$", str(int(process) + int(process2))):
+                            print("This equals {:.2f}".format((float(process) + float(process2))))
+                            break
+                        else:
+                            print("This equals {:,}".format(int(int(process) + int(process2))))
+                            break
+                    if sign == "2":
+                        if re.search(r"\d+\.\d{2,}$", str(int(process) - int(process2))):
+                            print("This equals {:.2f}".format((float(process) - float(process2))))
+                            break
+                        else:
+                            print("This equals {:,}".format(int(int(process) - int(process2))))
+                            break
+                    if sign == "3":
+                        if re.search(r"\d+\.\d{2,}$", str(int(process) * int(process2))):
+                            print("This equals {:.2f}".format((float(process) * float(process2))))
+                            break
+                        else:
+                            print("This equals {:,}".format(int(int(process) * int(process2))))
+                            break
+                    if sign == "4":
+                        if re.search(r"\d+\.\d{2,}$", str(int(process) / int(process2))):
+                            print("This equals {:.2f}".format((float(process) / float(process2))))
+                            break
+                        else:
+                            print("This equals {:,}".format(int(int(process) / int(process2))))
+                            break
+                    if sign == "5":
+                        if re.search(r"\d+\.\d{2,}$", str(int(process) ** int(process2))):
+                            print("This equals {:.2f}".format((float(process) ** float(process2))))
+                            break
+                        else:
+                            print("This equals {:,}".format(int(int(process) ** int(process2))))
+                            break
+            except ZeroDivisionError:
+                    print("Cannot divide by zero.")
+                    break
+        elif process == "":
+            if error_process < 3:
+                process = input("Please write something: ")
+                error_process += 1
+            elif error_process == 3:
+                print("Please try again later.")
+                break
+        else:
+            if error_process > 2:
+                print("Please try again later.")
+                break
+            elif error_process < 3:
+                process = input("Please use numbers only: ")
+                error_process += 1
+    if sign not in ["1", "2", "3", "4", "5"]:
+        return("You made wrong, please try again later.")
+
+Calculator()
